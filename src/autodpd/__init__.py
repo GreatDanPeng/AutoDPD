a85ae859c8283e12c2a119584ca0ab427111e0da
GreatDanPeng <1399756570@qq.com>
2025-01-14 17:38:12 -0500

    typo

--- src/autodpd/__init__.py
+++ src/autodpd/__init__.py
@@ -1,7 +1,7 @@
 # ============================================================================== #
 # MIT License                                                                    #
 #                                                                                #
-# Copyright (c) 2024 Dan Donsgen Peng                                            #
+# Copyright (c) 2024 Dan Dongshen Peng                                            #
 #                                                                                #
 # Permission is hereby granted, free of charge, to any person obtaining a copy   #
 # of this software and associated documentation files (the "Software"), to deal  #
